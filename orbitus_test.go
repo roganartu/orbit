2ee235e4aff6cd0d0eca65615352508d29dcd8d2
Tony Lee <github@roganartu.com>
2016-06-01 20:34:15 +1000

    Moved test vars to correct test file

--- orbitus_test.go
+++ orbitus_test.go
@@ -1,49 +1,14 @@
 package orbitus
 
 import (
-	"github.com/bmizerany/assert"
 	"testing"
+
+	"github.com/bmizerany/assert"
 )
 
 var (
 	buffer_size uint64 = 256 // 2^8
 	test               = "Test string"
-
-	receiverRan = false
-	receiver    = func(o Orbiter, ids []uint64) {
-		receiverRan = true
-		if orb, ok := o.(ReceiverOrbiter); ok {
-			orb.SetReceiverIndex(ids[0] + 1)
-		}
-	}
-	journalerRan = false
-	journaler    = func(o Orbiter, ids []uint64) {
-		journalerRan = true
-		if orb, ok := o.(ReceiverOrbiter); ok {
-			orb.SetJournalerIndex(ids[0])
-		}
-	}
-	replicatorRan = false
-	replicator    = func(o Orbiter, ids []uint64) {
-		replicatorRan = true
-		if orb, ok := o.(ReceiverOrbiter); ok {
-			orb.SetReplicatorIndex(ids[0])
-		}
-	}
-	unmarshallerRan = false
-	unmarshaller    = func(o Orbiter, ids []uint64) {
-		unmarshallerRan = true
-		if orb, ok := o.(ReceiverOrbiter); ok {
-			orb.SetUnmarshallerIndex(ids[0])
-		}
-	}
-	executorRan = false
-	executor    = func(o Orbiter, ids []uint64) {
-		executorRan = true
-		if orb, ok := o.(ReceiverOrbiter); ok {
-			orb.SetExecutorIndex(ids[0])
-		}
-	}
 )
 
 func TestGetMessage(t *testing.T) {
--- receiver_test.go
+++ receiver_test.go
@@ -6,6 +6,44 @@ import (
 	"time"
 )
 
+var (
+	receiverRan = false
+	receiver    = func(o Orbiter, ids []uint64) {
+		receiverRan = true
+		if orb, ok := o.(ReceiverOrbiter); ok {
+			orb.SetReceiverIndex(ids[0] + 1)
+		}
+	}
+	journalerRan = false
+	journaler    = func(o Orbiter, ids []uint64) {
+		journalerRan = true
+		if orb, ok := o.(ReceiverOrbiter); ok {
+			orb.SetJournalerIndex(ids[0])
+		}
+	}
+	replicatorRan = false
+	replicator    = func(o Orbiter, ids []uint64) {
+		replicatorRan = true
+		if orb, ok := o.(ReceiverOrbiter); ok {
+			orb.SetReplicatorIndex(ids[0])
+		}
+	}
+	unmarshallerRan = false
+	unmarshaller    = func(o Orbiter, ids []uint64) {
+		unmarshallerRan = true
+		if orb, ok := o.(ReceiverOrbiter); ok {
+			orb.SetUnmarshallerIndex(ids[0])
+		}
+	}
+	executorRan = false
+	executor    = func(o Orbiter, ids []uint64) {
+		executorRan = true
+		if orb, ok := o.(ReceiverOrbiter); ok {
+			orb.SetExecutorIndex(ids[0])
+		}
+	}
+)
+
 func TestNewReceiverOrbiter(t *testing.T) {
 	orbiter := NewReceiverOrbiter(buffer_size, nil, nil, nil, nil, nil)
 	var i uint64 = 4
